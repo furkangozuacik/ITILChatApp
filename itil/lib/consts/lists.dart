095b3cd7380155b58838b9a89e2b5f9bab34d3d0
furkangozuacik <gozuacikfurkan@gmaili.com>
2023-06-23 02:40:48 +0300

    Proje taşındı

---
+++ itil/lib/consts/lists.dart
@@ -0,0 +1,19 @@
+import 'package:itil/consts/consts.dart';
+import 'package:itil/consts/strings.dart';
+
+import 'images.dart';
+
+const categoriesList = [
+  "Designs",
+  "Strategy",
+  "Trancition",
+  "Operations",
+  "Continual İmprovement"
+];
+const categoryImages = [
+  imgFc1,
+  imgFc2,
+  imgFc3,
+  imgFc4,
+  imgFc5,
+];
